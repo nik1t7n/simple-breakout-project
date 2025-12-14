4b7ca51a261e57fc891f027bb6d29ad9c6519403
Nik1t7n <20nik.nosov21@gmail.com>
2025-12-14 09:30:00 +0900

    feat(graphics): expose screen metrics and declare new draw functions

--- graphics.h
+++ graphics.h
@@ -1,7 +1,17 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
+#include "raylib.h"
+
+// Expose metrics
+inline Vector2 screen_size;
+inline float screen_scale;
+inline float cell_size;
+inline Vector2 shift_to_center;
+
 void derive_graphics_metrics();
+
+void draw_text(const char* text, float x, float y, float size, Color color);
 
 void draw_menu();
 void draw_ui();
@@ -9,7 +19,9 @@ void draw_level();
 void draw_paddle();
 void draw_ball();
 void draw_pause_menu();
-void init_victory_menu();
 void draw_victory_menu();
+void draw_game_over_menu();
+
+void init_victory_menu();
 
 #endif // GRAPHICS_H
